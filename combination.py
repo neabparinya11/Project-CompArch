f799205ab42f62763af97f8af9a956cd9af4ed31
patthaveekarn <patthaveekarn2544@gmail.com>
2023-10-15 20:23:01 +0700

    stash

--- combination.py
+++ combination.py
@@ -1,12 +1,11 @@
 import math
 
 def combination(n,r):
-    return combination(n,r)
-
-n = "n"
-r = "r"
-combi = combination(n,r)
-
+    # กรณีพิเศษ: C(n,0) และ C(n,n) เท่ากับ 1
+    if r == 0 or r == n:
+        return 1
+    # ใช้สูตร combination(n,r) = combination(n-1,r) + combination(n-1,r-1)
+    return combination(n-1,r) + combination(n-1,r-1)
 class data:
     n = "n"
     r = "r"
